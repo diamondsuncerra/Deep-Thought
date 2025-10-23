cf798fcae05e6a1c0d1098a13947fb9c16ebab29
Cerasela-Nicola Soare <ceraselanicola@yahoo.com>
2025-10-23 12:32:18 +0300

    Added 5 options.

--- DeepThought/src/DeepThought/Util/ConsoleHelpers.cs
+++ DeepThought/src/DeepThought/Util/ConsoleHelpers.cs
@@ -24,23 +24,36 @@ namespace DeepThought.src.DeepThought.Util
 
         public static void DoOption1()
         {
-
+            Console.WriteLine("Please submit your Ultimate Questions for which we definitely have an answer.");
+            string? UltimateQuestion = Console.ReadLine();
+            // need to see if it's ok
+            Console.WriteLine("Which algorithm should Deep Thought use?");
+            string? Algorithm = Console.ReadLine();
+            // create job
         }
         public static void DoOption2()
         {
+            Console.WriteLine("Printing jobs..");
 
         }
         public static void DoOption3()
         {
-
+            Console.WriteLine("Please submit the JobId.");
+            string? JobIdString = Console.ReadLine();
+            if (int.TryParse(JobIdString, out int JobId))
+            {
+                Console.WriteLine("Printing Job Result..");
+            } else return;
+            
         }
         public static void DoOption4()
         {
-
+            Console.WriteLine("You've already found the answer, haven't you?");
+            Console.WriteLine("Forever deleting the Ultimate Job..");
         }
          public static void DoOption5()
         {
-            
+            Console.WriteLine("Thank you for using Deep Thought. 42 ms until termination.");
         }
     }
 }
