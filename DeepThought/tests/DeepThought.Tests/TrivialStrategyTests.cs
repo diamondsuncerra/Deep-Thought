67fd43e79ce1d9c7c9336bd736029bf7dcd76cac
Cerasela-Nicola Soare <ceraselanicola@yahoo.com>
2025-10-28 13:48:02 +0200

    Tests

---
+++ DeepThought/tests/DeepThought.Tests/TrivialStrategyTests.cs
@@ -0,0 +1,18 @@
+﻿using DeepThought.src.DeepThought.Strategies;
+using Xunit;
+
+public class CalculatorTests
+{
+    [Fact] // ← marks a single test
+    public async Task TrivialStrategy_Return42_AndReportsProgress()
+    {
+        var strat = new TrivialStrategy();
+        var progressValues = new List<int>();
+        var progress = new Progress<int>(p => progressValues.Add(p));
+
+        var result = await strat.AnswerQuestion(CancellationToken.None, progress);
+
+        Assert.Equal("42", result);
+        Assert.Contains(100, progressValues);
+    }
+}
